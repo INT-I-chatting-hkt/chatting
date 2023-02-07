217b881e9ecc8d929fd930fb542305d02a5d76b0
1Bronze <khdkkhdd@gmail.com>
2023-02-07 15:07:07 +0900

    Add Answer

--- src/main/java/com/example/IntI/domain/Answer.java
+++ src/main/java/com/example/IntI/domain/Answer.java
@@ -2,10 +2,12 @@ package com.example.IntI.domain;
 
 import com.example.IntI.chat.domain.Question;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -18,4 +20,8 @@ public class Answer {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id")
     private Question question;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
--- src/main/java/com/example/IntI/qna/domain/QnaDto.java
+++ src/main/java/com/example/IntI/qna/domain/QnaDto.java
@@ -1,26 +1,41 @@
 package com.example.IntI.qna.domain;
 
+import com.example.IntI.chat.domain.Question;
+import com.example.IntI.domain.Answer;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Data
 public class QnaDto {
 
     // 질문 정보
     private String questName;
-    private String questImage;
+    private String questImagePath;
     private String questTime;
     private String questContext;
 
     // 채택된 답변 정보
     private String answerName;
-    private String answerImage;
+    private String answerImagePath;
     private String answerTime;
     private String answerContext;
 
-    public QnaDto(String questName, String questImage, String questTime, String questContext) {
-        this.questName = questName;
-        this.questImage = questImage;
-        this.questTime = questTime;
-        this.questContext = questContext;
+    public QnaDto createQnaDto(Question question, Optional<Answer> answer) {
+
+        questName = question.getUser().getNickname();
+        questImagePath = question.getUser().getProfileUrl();
+        LocalDateTime qTime = question.getCreateTime();
+        questTime = qTime.getYear() + "-" + qTime.getMonth() + "-" + qTime.getDayOfMonth() + " " + qTime.getHour() + ":" + qTime.getMinute();
+        questContext = question.getContext();
+
+        answerName = answer.get().getUser().getNickname();
+        questImagePath = answer.get().getUser().getProfileUrl();
+        LocalDateTime aTime = answer.get().getCreateTime();
+        questTime = aTime.getYear() + "-" + aTime.getMonth() + "-" + aTime.getDayOfMonth() + " " + aTime.getHour() + ":" + aTime.getMinute();
+        questContext = answer.get().getContext();
+
+        return this;
     }
 }
