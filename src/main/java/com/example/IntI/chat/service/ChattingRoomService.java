58802cb1798ba8133ec149b2408cc1e41beb24fd
1Bronze <khdkkhdd@gmail.com>
2023-02-08 00:05:28 +0900

    Edit ChatRoom, User image

--- src/main/java/com/example/IntI/chat/service/ChattingRoomService.java
+++ src/main/java/com/example/IntI/chat/service/ChattingRoomService.java
@@ -2,18 +2,34 @@ package com.example.IntI.chat.service;
 
 import com.example.IntI.chat.repository.ChattingRoomRepository;
 import com.example.IntI.domain.ChattingRoom;
+import com.example.IntI.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ChattingRoomService {
     private final ChattingRoomRepository chattingRoomRepository;
+    private final List<String> roomImageList = new ArrayList<>(new ArrayList<>(
+            Arrays.asList("amazon.png", "apple.jpg", "google.jpg", "microsoft.png","samsung.png", "tesla.png")));
+
     @Transactional
     public void join(ChattingRoom chattingRoom){
         chattingRoomRepository.join(chattingRoom);
+        setProfile(chattingRoom);
+    }
+
+    public void setProfile(ChattingRoom chattingRoom) {
+        String dirPath = "/images/roomImage";
+        Collections.shuffle(roomImageList);
+        chattingRoom.setProfile(dirPath + "/" + roomImageList.get(0));
     }
 
     public ChattingRoom findOne(Long id){
